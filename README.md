2b065b6819736a9cb5974b489b294e2079c6e64a
Andrew Kunkel <andrew@wielding.net>
2020-11-22 20:16:20 -0500

    added better keyboard handling and updated docs

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 This is an example Powershell module using [WieldingAnsi](https://github.com/Wielding/WieldingAnsi) to create a very primitive `htop` like process viewer for Windows.
 
-It is known to work using [Windows Terminal](https://github.com/microsoft/terminal).  Any other console running Powershell may have unexpected behavior or not work at all.  For example the ASNI escape codes for hiding/showing the cursor do not work under the default Powershell Core console but work fine under [Windows Terminal](https://github.com/microsoft/terminal).  
+This project is currently my playground for what is possible using [WieldingAnsi](https://github.com/Wielding/WieldingAnsi) and helps determine what functionality I will add to that project.
+
+My intention is not to fully implement anything process related so don't expect an `htop` replacement here.  My goal is to implement text base UI features in a Powershell console script. I am just using `htop` as a reference since it has a nice console interface.  Maybe one day it will be a full featured process monitor but I doubt it.  I will probably move onto something else before it ever gets that far.  
+
+This module is known to work using [Windows Terminal](https://github.com/microsoft/terminal).  Any other console running Powershell may have unexpected behavior or not work at all.  For example the ASNI escape codes for hiding/showing the cursor do not work under the default Powershell Core console but work fine under [Windows Terminal](https://github.com/microsoft/terminal).  
 
 It implements some handy ANSI escape sequences such as:
 
@@ -10,6 +14,13 @@ It implements some handy ANSI escape sequences such as:
 2. Moving the cursor to the top corner of the screen to update the screen.
 3. Erasing lines to clear old output.
 4. Hiding the cursor upon execution and then restoring when upon exit.
+
+I have also implemented some basic keyboard shortcuts.
+
+* `Q`, `F10`, `CTRL-C` - Quit
+* `CTRL-P` - Sort by CPU percent
+* `CTRL-N` - Sort by Name
+* `CTRL-D` - Toggle sort direction (Ascending,Descending)
 
 To give it a try 
 
@@ -24,6 +35,3 @@ Show-ProcessExt -Continuous -SortProperty CPU -MinCpu 0.01 -SortDirection Descen
 
 This will display an `htop` like continuously updating screen with as many processes as can fit within your current console height and have at least 0.01% CPU usage.
 
-You can stop it at any time by hitting `Q` or `Ctrl-C` which will return you to your previous console screen state.
-
-This project is currently my playground for what is possible using [WieldingAnsi](https://github.com/Wielding/WieldingAnsi) and helps determine what functionality I will add to that project.
--- WieldingProcess.psm1
+++ WieldingProcess.psm1
@@ -1,5 +1,25 @@
 Import-Module WieldingAnsi
 
+enum SortProperty {
+    None
+    CPU
+    Name
+}
+
+enum SortDirection {
+    Ascending
+    Descending
+}
+
+enum KeyCommand {
+    Quit
+    SortCpu
+    SortName
+    ToggleDirection
+    ToggleColor
+}
+
+
 [console]::TreatControlCAsInput = $true
 
 class ProcessInfo {
@@ -54,17 +74,22 @@ function Get-ProcessExt {
     $pi
 }    
 
-enum SortProperty {
-    None
-    CPU
-    Name
-}
+function Get-KeyCommand {
+   param (
+       [object]$KeyMap,
+       [System.ConsoleKeyInfo]$key
+   ) 
 
-enum SortDirection {
-    Ascending
-    Descending
-}
+   $matchedModifier = $KeyMappings.ContainsKey($key.Modifiers)
 
+   if ($matchedModifier) {
+       return $KeyMappings[$key.Modifiers][$key.Key]
+   }
+
+   return $KeyMappings["None"][$key.Key]
+
+
+}
 function Show-ProcessExt {
     param (
         [string]$Name = "*",
@@ -75,7 +100,23 @@ function Show-ProcessExt {
         [switch]$Continuous
     )
 
-    $quitKeys = @([int][char]'Q', [int][char]'q', 3)
+
+    $KeyMappings = @{
+        "None" = @{
+            [ConsoleKey]::Q = [KeyCommand]::Quit
+            [ConsoleKey]::F10 = [KeyCommand]::Quit
+        }
+        [System.ConsoleModifiers]::Control = @{
+            [ConsoleKey]::C = [KeyCommand]::Quit
+            [ConsoleKey]::P = [KeyCommand]::SortCpu
+            [ConsoleKey]::N = [KeyCommand]::SortName
+            [ConsoleKey]::D = [KeyCommand]::ToggleDirection
+        }
+
+        ([System.ConsoleModifiers]::Shift + [System.ConsoleModifiers]::Alt + [System.ConsoleModifiers]::Control)  = @{
+            [ConsoleKey]::X = [KeyCommand]::Quit
+        }
+    }
 
     if ($Continuous) {
         Add-Member -InputObject $Wansi -MemberType NoteProperty -Name "EraseDisplay" -Value "`e[2J" -Force
@@ -139,7 +180,7 @@ function Show-ProcessExt {
                             (ConvertTo-AnsiString "{:F15:}$($p.Id){:R:}" -PadRight 10).Value,
                             (ConvertTo-AnsiString "{:F10:}$($p.CPU){:R:}" -PadLeft 8).Value,
                             (ConvertTo-AnsiString "{:F3:}$($p.Name){:R:}" -PadRight 30).Value,
-                            (ConvertTo-AnsiString "{:F8:}$($p.Description){:R:}{:EraseLine:}" -PadRight 40).Value.SubString(0, 40 + $Wansi.EraseLine.Length)
+                            (ConvertTo-AnsiString "{:F8:}$($p.Description){:R:}{:EraseLine:}" -PadRight 40).Value.SubString(0, 40)
                         )
                     }
                 }
@@ -155,14 +196,40 @@ function Show-ProcessExt {
             }            
 
             $moveToLastLine = "`e[$($host.Ui.RawUI.WindowSize.Height);0H"
-            Write-Wansi "$moveToLastLine{:F15:}{:B6:}'Q' or 'Ctrl-C' to quit{:EraseLine:}{:R:}"
+            Write-Wansi "$moveToLastLine{:F15:}{:B6:}'Q', 'F10' or 'Ctrl-C' to quit [$SortProperty`:$SortDirection]{:EraseLine:} {:R:}"
         }
 
-        if ($Host.UI.RawUI.KeyAvailable) { 
-            $keyHit = $Host.UI.RawUI.ReadKey("AllowCtrlC,IncludeKeyUp,NoEcho")
-            if ($quitKeys.Contains($keyHit.VirtualKeyCode)) {
-                Write-Wansi "{:DisableAlt:}{:ShowCursor:}"
-                return
+        if ([Console]::KeyAvailable) { 
+            # $keyHit = [Console]::ReadKey("AllowCtrlC,IncludeKeyUp,NoEcho")
+            $keyHit = [Console]::ReadKey("AllowCtrlC,IncludeKeyUp,NoEcho")
+
+            $kc = Get-KeyCommand $KeyMappings $keyHit
+
+            switch ($kc) {
+                ([KeyCommand]::Quit) {
+                    Write-Wansi "{:DisableAlt:}{:ShowCursor:}"
+                    return                                        
+                }
+
+                ([KeyCommand]::ToggleDirection) {
+                    if ($SortDirection -eq [SortDirection]::Ascending) {
+                        $SortDirection = [SortDirection]::Descending
+                    } else {
+                        $SortDirection = [SortDirection]::Ascending
+                    }
+                }
+
+                ([KeyCommand]::SortName) {
+                    $SortProperty = [SortProperty]::Name
+                }
+
+                ([KeyCommand]::SortCpu) {
+                    $SortProperty = [SortProperty]::CPU
+                }
+
+                ([KeyCommand]::ToggleColor) {
+                    $Wansi.Enabled = (-not $Wansi.Enabled )
+                }
             }
         }
     }
